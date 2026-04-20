ca1072952c5f3b67c4fc4367a99b368a181153a0
digvijay315 <digvijaykumar.315@gmail.com>
2026-04-20 14:26:45 +0530

    update'

--- pre-launch landing page/src/app/[locale]/(web)/page.tsx
+++ pre-launch landing page/src/app/[locale]/(web)/page.tsx
@@ -1,21 +1,27 @@
-// import { AgentsSection } from '@/components/web/AgentsSection'
-// import Brokerage from '@/components/web/Brokerage'
-// import { HeroSection } from '@/components/web/HeroSection'
-// import { IncentivesSection } from '@/components/web/IncentivesSection'
-// import PropertyIntent from '@/components/web/IntentSection'
-// import Joindeal from '@/components/web/Joindeal'
-// import OwnersInControl from '@/components/web/OwnersInControl'
-// import NotAPortalSection from '@/components/web/NotAPortalSection'
-// import { PortalSection } from '@/components/web/PortalSection'
-// import { PricingSection } from '@/components/web/PricingSection'
-// import PropertyManager from '@/components/web/PropertyManager'
-// import ConnectionVerification from '@/components/web/ConnectionVerification'
-// import { VerificationSection } from '@/components/web/VerificationSection'
-// import SubHeroSection from '@/components/web/SuBHeroSection'
-import SmarterFilters from '@/components/web/SmarterFilters'
+
+"use client"
+
+import dynamic from "next/dynamic";
+
+
+// import SmarterFilters from '@/components/web/SmarterFilters'
+
+const SmarterFilters = dynamic(() => import("@/components/web/SmarterFilters"), {
+  ssr: false,
+});
+
+
 import WhatsComing from '@/components/web/WhatsComing'
-import HeroSection from '@/components/web/HeroSection'
-import StartWithRequirements from '@/components/web/startwith_requirments'
+// import HeroSection from '@/components/web/HeroSection'
+
+const HeroSection = dynamic(() => import("@/components/web/HeroSection"), {
+  ssr: false,
+});
+// import StartWithRequirements from '@/components/web/startwith_requirments'
+
+const StartWithRequirements = dynamic(() => import("@/components/web/startwith_requirments"), {
+  ssr: false,
+});
 import ForAgents from '@/components/web/for_agents'
 import ForBrockerage from '@/components/web/for_brokerage'
 import RealstateWork from '@/components/web/realstatework'
